d5aad47222fcd24101876411550ca8aa3d0ad260
prostoG <49344670+prostoG@users.noreply.github.com>
2020-12-06 17:25:51 +0300

    add code

---
+++ js/accordion-main.js
@@ -0,0 +1,7 @@
+$( function() {
+    $( "#accordion" ).accordion( {
+        collapsible: true,
+        animate: 500,
+        icons: {"header": "ui-icon-plus", "activeHeader": "ui-icon-minus"}
+    }) 
+} );
