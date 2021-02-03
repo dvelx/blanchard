49413ac99fef1b509e2962c17f9710b86fa7e30c
prostoG <49344670+prostoG@users.noreply.github.com>
2021-02-03 20:56:22 +0300

    еще немного поправил - но все же не то совсем

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#open').addEventListener('click', function() {
         document.querySelector('#close').classList.toggle('is-active');
     });
+
 });
