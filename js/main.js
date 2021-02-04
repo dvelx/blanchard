9961cbcea246904ad58e745127563c194e399bf8
prostoG <49344670+prostoG@users.noreply.github.com>
2021-02-04 17:15:31 +0300

    feat: добавил плавность в выпадашки

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ window.addEventListener('DOMContentLoaded', function() {
         document.querySelector('#close').classList.toggle('is-active');
     });
 
-    document.querySelectorAll('.menu__item').forEach(function(menuItem) {
-        menuItem.addEventListener('click', function(event) {
-            const path = event.currentTarget.dataset.path
+    document.querySelectorAll('.menu__item').forEach(function(menuItem) {   //выбираем из документа нужный нам класс с forEach я еще не разобрался что это такое и как оно работает
+        menuItem.addEventListener('click', function(event) {                //добавляем к нашему выбранному классу обработчик событий click
+            const path = event.currentTarget.dataset.path                   //создаем const для того чтобы найти наш селектор по data в случае получится что будем искать по  data-path="какое то значение"
 
             dropDown = document.querySelector(`[data-target="${path}"]`);
 
