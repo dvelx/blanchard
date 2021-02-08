f656447035e58a8473ac397bc8300cdac056485e
prostoG <49344670+prostoG@users.noreply.github.com>
2021-02-08 21:16:06 +0300

    feat до блока event

--- js/main.js
+++ js/main.js
@@ -22,4 +22,19 @@ window.addEventListener('DOMContentLoaded', function() {
         })
     })
 
+
+    document.querySelectorAll('.country-btn').forEach(function(language) {   //выбираем из документа нужный нам класс с forEach я еще не разобрался что это такое и как оно работает
+        language.addEventListener('click', function(event) {                //добавляем к нашему выбранному классу обработчик событий click
+            const language = event.currentTarget.dataset.language                   //создаем const для того чтобы найти наш селектор по data в случае получится что будем искать по  data-path="какое то значение"
+
+            changeLanguage = document.querySelector(`[data-target="${language}"]`);
+
+            document.querySelectorAll('.catalog__description').forEach(function(tabClick) {
+                tabClick.classList.remove('catalog__description-active')
+            })
+            document.querySelector(`[data-target="${language}"]`).classList.add('catalog__description-active');
+
+        })
+    })
+
 });
