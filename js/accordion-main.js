f0382cca37989dd03c3b3e26568b2ac1f8ee6749
prostoG <49344670+prostoG@users.noreply.github.com>
2021-07-30 23:32:50 +0300

    fix

--- js/accordion-main.js
+++ js/accordion-main.js
@@ -1,14 +1,7 @@
 $( function() {
 	$( "#accordion" ).accordion( {
 		collapsible: true,
-		animate: 500,
-		icons: {"header": "ui-icon-plus", "activeHeader": "ui-icon-minus"}
-	}) 
-} );
-
-$( function() {
-	$( "#accordionTablet" ).accordion( {
-		collapsible: true,
+		heightStyle: "content",
 		animate: 500,
 		icons: {"header": "ui-icon-plus", "activeHeader": "ui-icon-minus"}
 	}) 
